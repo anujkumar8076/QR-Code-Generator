49d3c8ebd49c5f46eceaa5f5abcaa93f24e3866c
Anuj kumar <130743687+anujkumar8076@users.noreply.github.com>
2024-09-06 14:56:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let qrContainer = document.getElementById('qrContainer')
+let qrText = document.getElementById('qrText')
+let genBtn = document.getElementById('genBtn')
+let downloadBtn = document.getElementById('downloadBtn')
+let qrImage
+
+generateQR = (qrtext)=>{
+    qrContainer.innerHTML = ""
+    return new QRCode(qrContainer, {
+        text: qrtext,
+        width: 200,
+        height: 200,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+}
+
+downloadQR = ()=>{
+    qrImage = document.querySelector('#qrContainer img')
+
+    let imgSrc = qrImage.getAttribute('src')
+    console.log(imgSrc)
+    downloadBtn.setAttribute("href", imgSrc)
+}
+
+genBtn.addEventListener("click", (e)=>{
+    let text = qrText.value
+    if(text.length>0){
+
+        qrcode = generateQR(text)
+        qrContainer.classList.add('img-show')
+        downloadBtn.classList.add('download-active')
+
+    }
+})
+
+downloadBtn.addEventListener("click", downloadQR)
